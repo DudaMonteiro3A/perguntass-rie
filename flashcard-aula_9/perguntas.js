49f76ccaa9d4d9f2b7c2bb9a14a1ef5b29553379
DudaMonteiro3A <0000110415089xsp@al.educacao.sp.gov.br>
2024-11-28 18:11:40 -0300

    Add files via upload

---
+++ flashcard-aula_9/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Outer Banks',
+    'Qual é o nome do protagonista?',
+    'John B'
+)
+
+criaCartao(
+    'Outer Banks',
+    'Como se chama os grupos de Outer Banks?',
+    'Pougues e Kooks'
+)
+
+criaCartao(
+    'Outer Banks',
+    'Para aonde John B e Sarah vão na segunda temporada?',
+    'Bahamas'
+)
+
+criaCartao(
+    'Outer Banks',
+    'Quantos integrantes tem no grupo de John b?',
+    '5'
+)
